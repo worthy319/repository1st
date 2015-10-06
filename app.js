36b13acbd6f5030a04d72cd1774ba14de6d9b498
worthy319 <worthykobe@gmail.com>
2015-10-06 20:41:35 +0800

    Mac_1st
    Mac_1st_pull

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ var ejs = require("ejs");
 var bodyParser = require("body-Parser");
 
 // --> 定义静态资源文件请求的映射路径
-app.use("/resource", express.static())
-
-
+app.use("/resource", express.static(__dirname + "/resource"));
 
 
 
